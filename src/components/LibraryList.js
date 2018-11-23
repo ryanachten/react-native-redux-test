7551eb11d49804125773a4b732df1ecd9bd0c73f
Ryan Achten <ryanachten@gmail.com>
2018-11-24 10:08:06 +1300

    Add init Library reducer and LibraryList component

---
+++ src/components/LibraryList.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class LibraryList extends Component {
+  render() {
+    return;
+  }
+}
+
+export { LibraryList };
